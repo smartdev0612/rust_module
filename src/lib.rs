8c01ff14d25806db51d6cdc0520c2b072c8de2b5
Daniel Morales <goodluck1031@outlook.com>
2022-05-10 04:24:36 -0700

    custom smart pointer

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,48 @@
+/*
 pub mod file_1;
 pub mod file_2;
+*/
+
+//--------------------------------
+// Publishing your crate
+//--------------------------------
+/* 
+These lines are not going to be part of documentation
+*/
+
+//! # Basic math crate
+//! This is a collection of some generally used math functions
+//! 
+/// Computes a square of an input number
+/// 
+/// # Examples
+/// ```
+/// let n = 5;
+/// let answer = rust_module::square(n);
+/// assert_eq!(25, answer);
+/// ```
+/// 
+/// # Limitations
+/// 
+/// # Some other section
+
+pub fn square(num: i32) -> i32 {
+    num * num
+}
+
+/// Computes a cube of an input number
+/// 
+/// # Examples
+/// ```
+/// let n = 5;
+/// let answer = rust_module::cube(n);
+/// assert_eq!(125, answer);
+/// ```
+/// 
+/// # Limitations
+/// 
+/// # Some other section
+
+pub fn cube(num: i32) -> i32 {
+    num * num * num
+}
--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,7 @@ fn main() {
 //------------------------------------
 //  Using External Crates
 //------------------------------------
+/*
 use array_tool::vec::*;
 fn main() {
     let vec_1 = vec![1,2,3,1,5,3,5,2];
@@ -56,4 +57,115 @@ fn main() {
     println!("The union = {:?}", union_set);
 
     println!("Vec 2 three times displayed = {:?}", vec_2.times(3));
+}
+
+
+//------------------------------------
+//  Publishing Crate
+//------------------------------------
+fn main() {
+
+}
+*/
+
+
+//------------------------------------
+//  Smart Pointers
+//    - Box Smart Pointer
+//    - Use case of Box Smart Pointer
+//    - Custom Defined Smart Pointer
+//    - Deref coercion
+//------------------------------------
+
+/*
+fn main() {
+    let single_value = Box::new(0.625);
+    let x = 0.625;
+    println!("Are the value being equal {}", x == *single_value);
+
+    let mut stack_var = 4;
+    let stack_ref = &stack_var;
+
+    let heap_var = Box::new(stack_var);
+
+    stack_var = 5;
+    println!("The value of stack_var = {} and heap_var = {}", stack_var, heap_var);
+
+    let point = Box::new((100, 125));
+    println!("{} {}", 100 == point.0, point.1);
+
+    let x = *point;
+
+}
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, Option<Box<List>>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+
+fn main() {
+    let list = Cons(1, Some(Box::new(Cons(2, Some(Box::new(Cons(3, None)))))));
+    println!("{:?}", list);
+}
+*/
+
+struct MySmartPointer<T: std::fmt::Debug> {
+    value:T
+}
+
+impl<T: std::fmt::Debug> MySmartPointer<T> {
+    fn new(x:T) -> MySmartPointer<T> {
+        MySmartPointer { value: x}
+    }
+}
+
+
+use std::ops::Deref;
+
+impl<T: std::fmt::Debug> Deref for MySmartPointer<T>{
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.value
+    }
+}
+
+impl<T: std::fmt::Debug> Drop for MySmartPointer<T> {
+    fn drop(&mut self) {
+        println!("dropping mysmartpointer object from memory {:?}", self.value);
+    }
+}
+
+fn my_fn(str: &str) {
+    println!("The string received from the main is {}", str);
+}
+
+fn main() {
+    /*
+    let a = 50;
+    let b = Box::new(a);
+    println!("{}", 50 == a);
+    println!("{}", 50 == *b);
+    //println!("{}", a == b);
+
+    let sptr1 = MySmartPointer::new(a);
+    let sptr2 = MySmartPointer::new(*b);
+
+    println!("{}", a == *sptr1); // *(sptr1.deref())
+
+    // let z = *sptr1;
+
+    drop(sptr1);
+    */
+
+    let sptr_1 = MySmartPointer::new("Nouman Azam");
+    my_fn(&sptr_1);     // &mysmartpointer -> &String -> &str
+
+    let some_vec = MySmartPointer::new(vec![1,2,3]);
+
+    for z in &*some_vec {
+        println!("The value is {}", z);
+    }
 }
