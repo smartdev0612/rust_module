e2709f7932cbb9832b84aeb349d8593a8d0b5afc
Daniel Morales <goodluck1031@outlook.com>
2022-05-09 19:53:56 -0700

    external crates

--- src/file_2.rs
+++ src/file_2.rs
@@ -1,3 +1,25 @@
-pub fn printing() {
-    println!("Hello from file_2 crate");
+mod Person {
+    pub struct Personal_info {
+        pub age: u8,
+        pub education: String,
+    }
+
+    impl Personal_info {
+        pub fn new(new_edu: &str) -> Self {
+            Self {
+                education: String::from(new_edu),
+                age: 20,
+            }
+        }
+    }
+}
+
+pub fn some_person() {
+    let mut person1 = Person::Personal_info::new("Bachelors");
+    person1.education = String::from("masters");
+
+    let person2 = Person::Personal_info {
+        age: 42,
+        education: String::from("Masters"),
+    };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,59 @@
+//------------------------------------
+//  Rust Modules
+//------------------------------------
+/*
 use rust_module::file_1;
 use rust_module::file_2;
 
 fn main() {
     rust_module::file_1::printing();
     rust_module::file_2::printing();
+} 
+
+mod file_1;
+
+fn main() {
+    let rect1 = Rectangle {
+        length: 5,
+        width: 10,
+    };
+
+    file_1::rect_area(&rect1.length, &rect1.width);
+}
+
+struct Rectangle{
+    length: i32,
+    width: i32,
+}
+
+
+mod file_2;
+
+fn main() {
+    file_2::some_person();
+}
+
+
+mod file_3;
+
+fn main() {
+    file_3::allowance();
+}
+*/
+
+//------------------------------------
+//  Using External Crates
+//------------------------------------
+use array_tool::vec::*;
+fn main() {
+    let vec_1 = vec![1,2,3,1,5,3,5,2];
+    let vec_2 = vec![1,2,3];
+
+    let intersection = vec_1.intersect(vec_2.clone());
+    println!("The intersection = {:?}", intersection);
+
+    let union_set = vec_1.union(vec_2.clone());
+    println!("The union = {:?}", union_set);
+
+    println!("Vec 2 three times displayed = {:?}", vec_2.times(3));
 }
